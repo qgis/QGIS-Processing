7e70eef57ed241119347d579f865512cf47ff48a
igeofr <Florian@iMac-de-Florian.local>
2016-03-06 16:03:12 +0100

    Translate Cadastre_FR_WMS

--- scripts/Cadastre_FR_WMS.py
+++ scripts/Cadastre_FR_WMS.py
@@ -1,24 +1,24 @@
 ##Cadastre FR=group
-##Cadastre FR - WMS - Ajout du cadastre=name
+##Cadastre FR - WMS - Add a cadastral map=name
 
-##Couche_commune=vector
-##Champ_code_INSEE=field Couche_commune
-##Champ_nom_de_commune=field Couche_commune
-##Code_EPSG=string 2154
+##Vector_layer_of_communes=vector
+##INSEE_code=field Vector_layer_of_communes
+##Commune_name=field Vector_layer_of_communes
+##EPSG_code=string 2154
 
 from qgis.core import QgsRasterLayer,QgsMapLayerRegistry
 from qgis.utils import iface,QgsMessageBar
 
-layer = processing.getObject(Couche_commune)
+layer = processing.getObject(Vector_layer_of_communes)
 
-if Code_EPSG == '2154' or Code_EPSG == '3942' or Code_EPSG == '3943' or Code_EPSG == '3944' or Code_EPSG == '3945' or Code_EPSG == '3946' or Code_EPSG == '3947' or Code_EPSG == '3948' or Code_EPSG == '3949' or Code_EPSG == '3950' or Code_EPSG == '32630' or Code_EPSG == ' 32631' or Code_EPSG == '32632' or Code_EPSG == '3857' or Code_EPSG == '4326' or Code_EPSG == '4258' or Code_EPSG == '32620' or Code_EPSG == '2970' or Code_EPSG == '2972' or Code_EPSG == '2973' or Code_EPSG == '2975' or Code_EPSG == '32622' or Code_EPSG == '32740' or Code_EPSG == '32738' or Code_EPSG == '4471' or Code_EPSG == '32621' :
-    progress.setText(u'Systeme de coordonnees - EPSG : ' + Code_EPSG)
+if EPSG_code == '2154' or EPSG_code == '3942' or EPSG_code == '3943' or EPSG_code == '3944' or EPSG_code == '3945' or EPSG_code == '3946' or EPSG_code == '3947' or EPSG_code == '3948' or EPSG_code == '3949' or EPSG_code == '3950' or EPSG_code == '32630' or EPSG_code == ' 32631' or EPSG_code == '32632' or EPSG_code == '3857' or EPSG_code == '4326' or EPSG_code == '4258' or EPSG_code == '32620' or EPSG_code == '2970' or EPSG_code == '2972' or EPSG_code == '2973' or EPSG_code == '2975' or EPSG_code == '32622' or EPSG_code == '32740' or EPSG_code == '32738' or EPSG_code == '4471' or EPSG_code == '32621' :
+    progress.setText(u'EPSG code : ' + EPSG_code)
 
     tab = []
 
     for f in layer.getFeatures():
 
-        col_select =str(f[Champ_code_INSEE]),f[Champ_nom_de_commune]
+        col_select =str(f[INSEE_code]),f[Commune_name]
         tab.append(col_select)
 
         #Permet la suppression des doublons
@@ -28,22 +28,22 @@ if Code_EPSG == '2154' or Code_EPSG == '3942' or Code_EPSG == '3943' or Code_EPS
     for c_insee, n_couche in Lt  :
 
         #AMORCES_CAD,LIEUDIT,CP.CadastralParcel,SUBFISCAL,CLOTURE,DETAIL_TOPO,HYDRO,VOIE_COMMUNICATION,BU.Building,BORNE_REPERE
-        urlWithParams = "url=http://inspire.cadastre.gouv.fr/scpc/"+c_insee+".wms?contextualWMSLegend=0&crs=EPSG:"+Code_EPSG+"&dpiMode=7&featureCount=10&format=image/png&layers=AMORCES_CAD&layers=LIEUDIT&layers=CP.CadastralParcel&layers=SUBFISCAL&layers=CLOTURE&layers=DETAIL_TOPO&layers=HYDRO&layers=VOIE_COMMUNICATION&layers=BU.Building&layers=BORNE_REPERE&styles=&styles=&styles=&styles=&styles=&styles=&styles=&styles=&styles=&styles="
+        urlWithParams = "url=http://inspire.cadastre.gouv.fr/scpc/"+c_insee+".wms?contextualWMSLegend=0&crs=EPSG:"+EPSG_code+"&dpiMode=7&featureCount=10&format=image/png&layers=AMORCES_CAD&layers=LIEUDIT&layers=CP.CadastralParcel&layers=SUBFISCAL&layers=CLOTURE&layers=DETAIL_TOPO&layers=HYDRO&layers=VOIE_COMMUNICATION&layers=BU.Building&layers=BORNE_REPERE&styles=&styles=&styles=&styles=&styles=&styles=&styles=&styles=&styles=&styles="
 
         rlayer = QgsRasterLayer(urlWithParams, 'Cadastre_'+n_couche+'_'+c_insee, 'wms')
 
-        progress.setText(u'Nom de la commune : ' + n_couche+' - '+c_insee)
-        progress.setText(u'Validite du flux : %s' % rlayer.isValid())
+        progress.setText(u'Commune name : ' + n_couche+' - '+c_insee)
+        progress.setText(u'Validity of WMS : %s' % rlayer.isValid())
 
         QgsMapLayerRegistry.instance().addMapLayer(rlayer)
 
         if  rlayer.isValid() == True :
-            iface.messageBar().pushMessage("Information :", "Ajout du flux WMS pour la commune : "+n_couche, QgsMessageBar.INFO, duration=5)
+            iface.messageBar().pushMessage("Information :", "Adding a cadastral map : "+n_couche, QgsMessageBar.INFO, duration=5)
             iface.mapCanvas().refresh()
 
         else :
-            iface.messageBar().pushMessage("Warning :", "WMS invalide pour la commune : "+n_couche, QgsMessageBar.WARNING, duration=15)
+            iface.messageBar().pushMessage("Warning :", "WMS invalid : "+n_couche, QgsMessageBar.WARNING, duration=15)
 
 else :
-    iface.messageBar().pushMessage("Warning :", "EPSG inconnu", QgsMessageBar.WARNING, duration=15)
-    progress.setText(u'EPSG inconnu')
+    iface.messageBar().pushMessage("Warning :", "EPSG is unknown ", QgsMessageBar.WARNING, duration=15)
+    progress.setText(u'EPSG is unknown ')
