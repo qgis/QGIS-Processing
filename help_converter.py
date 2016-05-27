70fc43bc13caf470418190dbba39293870170320
rldhont <rldhont@gmail.com>
2016-05-27 21:51:06 +0200

    Add models and rscripts to help_converter.py

--- help_converter.py
+++ help_converter.py
@@ -2,11 +2,20 @@ import glob
 import pickle
 import json
 
+def convertHelp( filePath ):
+    with open(filePath, 'rb') as f:
+        try:
+            descriptions = pickle.load(f)
+            with open(filePath, 'w') as f:
+                f.write(json.dumps(descriptions))
+        except :
+            pass
+
 for filename in glob.glob('scripts/*.help'):
-	with open(filename, 'rb') as f:
-		try: 
-		    descriptions = pickle.load(f)
-		    with open(filename, 'w') as f:
-		        f.write(json.dumps(descriptions))
-		except :
-		    pass
+    convertHelp( filename )
+
+for filename in glob.glob('rscripts/*.help'):
+    convertHelp( filename )
+
+for filename in glob.glob('models/*.help'):
+    convertHelp( filename )
